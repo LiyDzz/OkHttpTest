d27c61d0b7ae01b37ab7e1330c2c0292f42b5210
LENOVO <www.1411707895@qq.com>
2017-05-09 10:53:10 +0800

    javaBean

---
+++ app/src/main/java/com/example/lenovo/okhttptest/Demo.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.okhttptest;
+
+import android.util.Log;
+
+/**
+ * Created by 李艳东 on 2017/5/9.
+ */
+
+public class Demo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        Log.e("tag", "nihao");
+        Log.e("tag", "nihao");
+        Log.e("tag", "nihao");
+        Log.e("tag", "nihao");
+        Log.e("tag", "nihao");
+    }
+
+
+}
