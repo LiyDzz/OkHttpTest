ed8c5be8d0296c512ff35eea9a4f0bfa50c033a8
LENOVO <www.1411707895@qq.com>
2017-05-09 10:37:27 +0800

    javaBean

---
+++ app/src/main/java/com/example/lenovo/okhttptest/Person.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.okhttptest;
+
+/**
+ * Created by 李艳东 on 2017/5/9.
+ */
+
+public class Person {
+    private String name;
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
